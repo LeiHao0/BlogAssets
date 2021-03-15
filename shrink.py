e2382db1fe962e7400b4d355de6ddc28ab5f54a2
leihao <leihao.sg@gmail.com>
2021-03-16 00:58:54 +0800

    rename extension from jpg to webp

--- shrink.py
+++ shrink.py
@@ -1,11 +1,14 @@
 # py /Users/lei/Movies/tmp/shrink.py namePrefix
 
+from subprocess import call
+from multiprocessing.dummy import Pool
+from functools import partial
 import os
 import subprocess
 import sys
 
-path =  os.path.dirname(__file__) + "./new"
-namePrefix=sys.argv[1]
+path = os.path.dirname(__file__) + "./new"
+namePrefix = sys.argv[1]
 
 os.chdir(path)
 # subprocess.run(["mkdir", "-p", "to"])
@@ -17,18 +20,16 @@ for file in files:
     if file.endswith('.jpeg'):
         filename = os.path.splitext(file)[0]
         newFilename = filename
-        
+
         if namePrefix:
             newFilename = namePrefix + "-" + str(i).zfill(2)
             i += 1
 
-        convertList.append(f"convert -resize x1080 -strip -quality 80% \"{file}\" \"../{newFilename}.jpg\"")
+        convertList.append(
+            f"convert -resize x1080 -strip -quality 80% \"{file}\" \"../{newFilename}.webp\"")
 
-from functools import partial
-from multiprocessing.dummy import Pool
-from subprocess import call
 
 pool = Pool(10)
 for i, returncode in enumerate(pool.imap(partial(call, shell=True), convertList)):
     if returncode != 0:
-       print("%d command failed: %d" % (i, returncode))
+        print("%d command failed: %d" % (i, returncode))
